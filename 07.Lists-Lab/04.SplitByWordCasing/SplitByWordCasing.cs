a50390dbf7d73ce6bacdc00b7dec90b88340540a
whywherewhat <mdsektor@gmail.com>
2017-06-14 22:25:13 +0100

    Added 04.SplitByWordCasing

---
+++ 07.Lists-Lab/04.SplitByWordCasing/SplitByWordCasing.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SplitByWordCasing
+{
+    class SplitByWordCasing
+    {
+        static void Main(string[] args)
+        {
+            List<string> text = Console.ReadLine().Split(new char[] { ',', ';', ':', '.', '!', '(', ')', '"', '\'', '\\', '/', '[', ']', ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+            List<string> lowercaseWords = new List<string>();
+            List<string> uppercaseWords = new List<string>();
+            List<string> allOther = new List<string>();
+
+            for (int index = 0; index < text.Count; index++)
+            {
+                if (text[index].All(char.IsLower))
+                {
+                    lowercaseWords.Add(text[index]);
+                }
+                else if (text[index].All(char.IsUpper))
+                {
+                    uppercaseWords.Add(text[index]);
+                }
+                else
+                {
+                    allOther.Add(text[index]);
+                }
+            }
+            Console.WriteLine("Lower-case: " + string.Join(", ", lowercaseWords));
+            Console.WriteLine("Mixed-case: " + string.Join(", ", allOther));
+            Console.WriteLine("Upper-case: " + string.Join(", ", uppercaseWords));
+        }
+    }
+}
