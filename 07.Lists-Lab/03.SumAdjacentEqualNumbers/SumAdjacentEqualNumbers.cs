249e76f8726abe310da81ce7089f5e701b1ffd56
whywherewhat <mdsektor@gmail.com>
2017-06-14 21:33:47 +0100

    Added 03.SumAdjacentEqualNumbers

---
+++ 07.Lists-Lab/03.SumAdjacentEqualNumbers/SumAdjacentEqualNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.SumAdjacentEqualNumbers
+{
+    class SumAdjacentEqualNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+
+            decimal sum = 0m;
+            int index = 1;
+
+            while (index < numbers.Count)
+            {
+                if (numbers[index - 1] == numbers[index])
+                {
+                    sum = numbers[index - 1] + numbers[index];
+                    numbers.RemoveAt(index - 1);
+                    numbers.RemoveAt(index - 1);
+                    numbers.Insert(index - 1, sum);
+                    if (index > 1)
+                    {
+                        index--;
+                    }
+                }
+                else
+                {
+                    index++;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
