c3d8b977dd0cf816a1fd159acc61d16493daf32b
whywherewhat <mdsektor@gmail.com>
2017-06-14 20:27:54 +0100

    Added 02.Append Lists

---
+++ 07.Lists-Lab/02.AppendLists/AppendLists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AppendLists
+{
+    class AppendLists
+    {
+        static void Main(string[] args)
+        {
+            List<string> results = new List<string>();
+            string[] input = Console.ReadLine().Split('|').ToArray();
+            
+            for (int index = input.Length - 1; index >= 0; index--)
+            {
+                var tokens = input[index].Split(' ').ToArray();
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    if (tokens[i] != "")
+                    {
+                        results.Add(tokens[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", results));
+        }
+    }
+}
