e5b5c84d483d0307716be52eb23fdbd55173d07a
whywherewhat <mdsektor@gmail.com>
2017-06-14 23:24:10 +0100

    Added 07.CountNumbers

---
+++ 07.Lists-Lab/07.CountNumbers/CountNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.CountNumbers
+{
+    class CountNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Array.Sort(numbers);
+
+            int count = 1;
+            for (int index = 1; index < numbers.Length; index++)
+            {
+                if (numbers[index - 1] == numbers[index])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine(numbers[index - 1] + " -> " + count);
+                    count = 1;
+                }
+            }
+            Console.WriteLine(numbers[numbers.Length - 1] + " -> " + count);
+        }
+    }
+}
