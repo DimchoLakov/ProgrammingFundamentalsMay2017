9f5403aaa40f9b6132d24f1b9c08c254cbe2d2b7
whywherewhat <mdsektor@gmail.com>
2017-06-14 19:42:35 +0100

    Added 01.RemoveNegativesAndReverse

---
+++ 07.Lists-Lab/01.RemoveNegativesAndReverse/RemoveNegativesAndReverse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.RemoveNegativesAndReverse
+{
+    class RemoveNegativesAndReverse
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            List<int> removedNegatives = new List<int>(numbers.Count);
+
+            for (int currentIndex = 0; currentIndex < numbers.Count; currentIndex++)
+            {
+                if (numbers[currentIndex] > 0)
+                {
+                    removedNegatives.Add(numbers[currentIndex]);
+                }
+            }
+            removedNegatives.Reverse();
+            if (removedNegatives.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", removedNegatives));
+            }
+            else
+            {
+                Console.WriteLine("empty");
+            }
+        }
+    }
+}
