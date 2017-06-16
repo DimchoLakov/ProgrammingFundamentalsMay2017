919e542e83de4394ae09dc4d4dda65abb1a5f7d8
whywherewhat <mdsektor@gmail.com>
2017-06-16 23:53:59 +0100

    Added 08.Dictionaries.Lambda.LINQ-Lab

---
+++ 08.Dictionaries.Lambda.LINQ/03.LINQ.SumMinMaxAverage/SumMinMaxAverage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.LINQ.SumMinMaxAverage
+{
+    class SumMinMaxAverage
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] numbers = new double[n];
+
+            List<double> list = new List<double>(numbers);
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                list[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Sum = {list.Sum()}");
+            Console.WriteLine($"Min = {list.Min()}");
+            Console.WriteLine($"Max = {list.Max()}");
+            Console.WriteLine($"Average = {list.Average()}");
+        }
+    }
+}
