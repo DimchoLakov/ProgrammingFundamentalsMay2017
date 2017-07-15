643f5a81f2c9c69460011ae86034507560fe583a
whywherewhat <mdsektor@gmail.com>
2017-07-15 21:57:43 +0100

    Added 06.Byte Flip

---
+++ 08.DictionariesAndsLists-MoreExercises/06.ByteFlip/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.ByteFlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Where(x => x.Length == 2).ToArray();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                char[] charArray = input[i].ToCharArray();
+                Array.Reverse(charArray);
+                Console.Write(System.Convert.ToChar(System.Convert.ToUInt32(new string(charArray), 16)));
+            }
+            Console.WriteLine();
+        }
+    }
+}
