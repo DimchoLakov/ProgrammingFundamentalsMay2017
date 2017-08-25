71297585b16ff1708191618451ea5e02f6ae2040
whywherewhat <mdsektor@gmail.com>
2017-08-25 19:11:14 +0100

    Added 09.ObjectsAndClasses-Lab

---
+++ 09.ObjectsAndClasses-Lab/02.RandomizeWords/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace _02.RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] words = input.Split(' ');
+
+            Random rnd = new Random();
+
+            for (int pos1 = 0; pos1 < words.Length; pos1++)
+            {
+                int pos2 = rnd.Next(0, words.Length - 1);
+
+                string temp = words[pos1];
+                words[pos1] = words[pos2];
+                words[pos2] = temp;
+            }
+            Console.WriteLine(string.Join($"{Environment.NewLine}", words));
+        }
+    }
+}
---
+++ 09.ObjectsAndClasses-Lab/04.DistanceBetweenPoints/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.DistanceBetweenPoints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstPoint = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] secondPoint = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            double distance = FindEuclideanDistance(firstPoint, secondPoint);
+            Console.WriteLine($"{distance:f3}");
+        }
+
+        static double FindEuclideanDistance(int[] firstPoint, int[] secondPoint)
+        {
+            int x1 = firstPoint[0];
+            int y1 = firstPoint[1];
+            int x2 = secondPoint[0];
+            int y2 = secondPoint[1];
+
+            double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+            return distance;
+        }
+    }
+}
