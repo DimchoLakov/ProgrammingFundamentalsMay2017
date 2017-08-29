4edec5a4a141e6ddefe2025968f7eb9572f6acf5
whywherewhat <mdsektor@gmail.com>
2017-08-29 23:46:39 +0100

    Added 04.Palindromes

---
+++ 11.StringsAndTextProcessing-Lab/04.Palindromes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedSet<string> palindromes = new SortedSet<string>();
+
+            char[] delimiters = new char[] {',', '.', '!', '?', ' '};
+
+            string[] words = Console.ReadLine().Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (string word in words)
+            {
+                if (isPalindrome(word))
+                {
+                    palindromes.Add(word);
+                }
+            }
+            Console.WriteLine(string.Join(", ", palindromes));
+        }
+
+        static bool isPalindrome(string word)
+        {
+            string original = word;
+            string reversed = new string(original.Reverse().ToArray());
+            bool isPalindrome = original == reversed;
+            return isPalindrome;
+        }
+    }
+}
