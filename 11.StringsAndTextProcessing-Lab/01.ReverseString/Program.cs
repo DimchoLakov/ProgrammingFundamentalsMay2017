d4422340f45886013af841341c559561ce0ad886
whywherewhat <mdsektor@gmail.com>
2017-08-29 23:18:05 +0100

    Added 01.ReverseString

--- 11.StringsAndTextProcessing-Lab/01.ReverseString/Program.cs
+++ 11.StringsAndTextProcessing-Lab/01.ReverseString/Program.cs
@@ -10,6 +10,7 @@ namespace _01.ReverseString
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(string.Join("", Console.ReadLine().ToCharArray().Reverse()));
         }
     }
 }
