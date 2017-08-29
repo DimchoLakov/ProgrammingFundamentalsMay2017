efaf2f3ebdc8578eb8911b0d9a968ffafc543912
whywherewhat <mdsektor@gmail.com>
2017-08-29 23:37:15 +0100

    Added 03.TextFilter

---
+++ 11.StringsAndTextProcessing-Lab/03.TextFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine()
+                .Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+
+            string input = Console.ReadLine();
+            
+            foreach (string bannedWord in bannedWords)
+            {
+                input = input.Replace($"{bannedWord}", new string('*', bannedWord.Length));
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
