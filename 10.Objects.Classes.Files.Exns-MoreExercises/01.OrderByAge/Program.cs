33ba968444bca0ee888a8e413e72911b44051d56
whywherewhat <mdsektor@gmail.com>
2017-08-29 19:08:47 +0100

    Added 01.OrderByAge

---
+++ 10.Objects.Classes.Files.Exns-MoreExercises/01.OrderByAge/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.OrderByAge
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+
+        public static Person Parse(string name, string id, int age)
+        {
+            return new Person
+            {
+                Name = name,
+                Id = id,
+                Age = age
+            };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            List<Person> people = new List<Person>();
+
+            while (input != "End")
+            {
+                string[] tokens = input.Split(' ');
+
+                string name = tokens[0];
+                string id = tokens[1];
+                int age = int.Parse(tokens[2]);
+
+                people.Add(Person.Parse(name, id, age));
+
+                input = Console.ReadLine();
+            }
+
+            foreach (Person person in people.OrderBy(a => a.Age))
+            {
+                Console.WriteLine($"{person.Name} with ID: {person.Id} is {person.Age} years old.");
+            }
+        }
+    }
+}
