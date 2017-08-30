a824fbc32bee393a08a28668c5f8e37fdb4916c8
whywherewhat <mdsektor@gmail.com>
2017-08-30 23:39:03 +0100

    Added 02.PhoneNumber

---
+++ 12.Regex-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string phonePattern = @"\+359( |\-)2\1\d{3}\1\d{4}\b";
+
+            Regex phoneRegex = new Regex(phonePattern);
+
+            MatchCollection phoneMatches = phoneRegex.Matches(input);
+
+            string[] matches = phoneMatches.Cast<Match>().Select(p => p.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
