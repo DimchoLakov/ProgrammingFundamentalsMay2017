18c663c7671c328e15b3ddc65c60bac37d13171d
whywherewhat <mdsektor@gmail.com>
2017-08-31 00:32:38 +0100

    Added 06.ReplaceATag

---
+++ 12.Regex-Lab/06.ReplaceATag/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _06.ReplaceATag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string tagPattern = @"<a.*?href.*?=(.*?)>(.*?)<\/a>";
+            string replacePattern = @"[URL href=$1]$2[/URL]";
+
+            while (input != "end")
+            {
+                string result = Regex.Replace(input, tagPattern, replacePattern);
+
+                Console.WriteLine(result);
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
