67c5b30dcb9c5b7bff93435b01ca7be025a9a61d
whywherewhat <mdsektor@gmail.com>
2017-08-30 23:44:06 +0100

    Added 03.MatchHexadecimalNumbers

---
+++ 12.Regex-Lab/03.MatchHexadecimalNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _03.MatchHexadecimalNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string hexPattern = @"\b(0x)*[A-F0-9]+\b";
+
+            Regex hexRegex = new Regex(hexPattern);
+
+            MatchCollection hexMatches = hexRegex.Matches(input);
+
+            string[] matches = hexMatches.Cast<Match>().Select(h => h.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
