fc7713c0b0a31bbfbee252ca25e299a5b2c6837b
whywherewhat <mdsektor@gmail.com>
2017-08-31 00:51:02 +0100

    Added 12.Regex-Exercises Added 01.ExtractEmails

---
+++ 12.Regex-Exercises/01.ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.ExtractEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string emailPattern = @"(?<=\s)([a-z]+|\d+)(\d+|\w+|\.+|-+)([a-z]+|\d+)\@[a-z\-]+\.[a-z]+(\.[a-z]+)*";
+
+            Regex emailRegex = new Regex(emailPattern);
+
+            MatchCollection emailMatches = emailRegex.Matches(input);
+
+            foreach (Match email in emailMatches)
+            {
+                Console.WriteLine(email.Value);
+            }
+        }
+    }
+}
