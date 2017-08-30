55b5ee133d8b755f634e7655c6eeb32b14d8d257
whywherewhat <mdsektor@gmail.com>
2017-08-30 23:58:25 +0100

    Added 04.MatchDates

---
+++ 12.Regex-Lab/04.MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _04.MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string datesPattern = @"\b(?<days>\d{2})(?<deli>[\/\-\.])(?<month>[A-Z][a-z]{2})\k<deli>(?<year>\d{4})\b";
+
+            Regex dateRegex = new Regex(datesPattern);
+
+            MatchCollection dateMatches = dateRegex.Matches(input);
+
+            foreach (Match date in dateMatches)
+            {
+                Console.WriteLine($"Day: {date.Groups["days"].Value}, Month: {date.Groups["month"].Value}, Year: {date.Groups["year"].Value}");
+            }
+        }
+    }
+}
