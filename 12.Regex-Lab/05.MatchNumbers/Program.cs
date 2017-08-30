56911da67f684cdba6e2bd0cac5a31b469390d33
whywherewhat <mdsektor@gmail.com>
2017-08-31 00:04:48 +0100

    Added 05.MatchNumbers

---
+++ 12.Regex-Lab/05.MatchNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _05.MatchNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string numberPattern = @"(^|(?<=\s))\-?\d+(\.\d+)*($|(?=\s))";
+
+            Regex numberRegex = new Regex(numberPattern);
+
+            MatchCollection numberMatches = numberRegex.Matches(input);
+
+            string[] matches = numberMatches.Cast<Match>().Select(n => n.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
