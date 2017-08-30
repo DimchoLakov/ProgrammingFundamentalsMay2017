26b6936d1c857a4bf6e4d34421a39ebde932ae5a
whywherewhat <mdsektor@gmail.com>
2017-08-30 23:27:22 +0100

    Added 12.Regex-Lab Added 01.MatchFullName

---
+++ 12.Regex-Lab/01.MatchFullName/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01.MatchFullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namePattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
+
+            Regex nameRegex = new Regex(namePattern);
+
+            string input = Console.ReadLine();
+
+            MatchCollection nameMatches = nameRegex.Matches(input);
+
+            string[] matches = nameMatches.Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
