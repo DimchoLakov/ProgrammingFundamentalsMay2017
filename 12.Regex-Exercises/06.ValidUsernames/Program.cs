f848130117c2aa67392905a27388dafb16160cda
whywherewhat <mdsektor@gmail.com>
2017-09-02 03:30:28 +0100

    Added 06.ValidUsernames

---
+++ 12.Regex-Exercises/06.ValidUsernames/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _06.ValidUsernames
+{
+    class User
+    {
+        public string Name { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] tokens = input.Split(new char[] { ' ', '/', '\\', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+
+            string pattern = @"\b([a-zA-Z]{1}[A-Za-z_0-9]{2,24})\b";
+
+            Regex userRegex = new Regex(pattern);
+
+            List<string> result = new List<string>();
+
+            foreach (string user in tokens)
+            {
+                if (userRegex.IsMatch(user))
+                {
+                    result.Add(user);
+                }
+            }
+
+            int maxSum = 0;
+
+            string firstUser = string.Empty;
+            string secondUser = string.Empty;
+
+            for (int i = 1; i < result.Count; i++)
+            {
+                int currentSum = result[i - 1].Length + result[i].Length;
+
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                    firstUser = result[i - 1];
+                    secondUser = result[i];
+                }
+            }
+
+            Console.WriteLine(firstUser);
+            Console.WriteLine(secondUser);
+        }
+    }
+}
