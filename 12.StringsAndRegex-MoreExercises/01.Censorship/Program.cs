3ff7df12d81658b04fa530213f124b47500399bb
whywherewhat <mdsektor@gmail.com>
2017-09-02 21:49:20 +0100

    Added 12.StringsAndRegex-MoreExercises Added 01.Censorship

---
+++ 12.StringsAndRegex-MoreExercises/01.Censorship/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.Censorship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string replaceWord = Console.ReadLine();
+            string sentence = Console.ReadLine();
+
+            sentence = sentence.Replace(replaceWord, new string('*', replaceWord.Length));
+            Console.WriteLine(sentence);
+        }
+    }
+}
