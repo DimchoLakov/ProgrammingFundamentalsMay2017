243f35b6e0b3be6c055cdc1ccc466c0c6bc43ea2
whywherewhat <mdsektor@gmail.com>
2017-09-02 21:55:32 +0100

    Added 02.EmailMe

---
+++ 12.StringsAndRegex-MoreExercises/02.EmailMe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.EmailMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = Console.ReadLine();
+            string[] tokens = email.Split('@');
+
+            string firstPart = tokens[0];
+            string secondPart = tokens[1];
+
+            int firstSum = firstPart.Sum(a => a);
+            int secondSum = secondPart.Sum(a => a);
+            int diff = firstSum - secondSum;
+
+            if (diff >= 0)
+            {
+                Console.WriteLine($"Call her!");
+            }
+            else
+            {
+                Console.WriteLine($"She is not the one.");
+            }
+
+        }
+    }
+}
