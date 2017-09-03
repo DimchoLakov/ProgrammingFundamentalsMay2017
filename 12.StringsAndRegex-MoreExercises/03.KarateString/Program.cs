851d431d720baee6bfe8ceffed9a0697cff2a066
whywherewhat <mdsektor@gmail.com>
2017-09-03 16:13:32 +0100

    Added 03.KarateStrings

---
+++ 12.StringsAndRegex-MoreExercises/03.KarateString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _03.KarateString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder(Console.ReadLine());
+            int strength = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '>')
+                {
+                    strength += int.Parse(input[i + 1].ToString());
+                    i++;
+
+                    while (i < input.Length && strength > 0)
+                    {
+                        if (input[i] == '>')
+                        {
+                            break;
+                        }
+                        input.Remove(i, 1);
+                        strength--;
+                    }
+                    i--;
+                }
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
