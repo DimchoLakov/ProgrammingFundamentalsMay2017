998af0bfc947cb630438d22f2b3c8878d094a9ec
whywherewhat <mdsektor@gmail.com>
2017-09-05 23:54:02 +0100

    Added 04.MorseCodeUpgraded

---
+++ 12.StringsAndRegex-MoreExercises/04.MorseCodeUpgraded/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _04.MorseCodeUpgraded
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] message = Console.ReadLine().Split(new char[] {'|'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            long sum = 0L;
+            string result = string.Empty;
+            foreach (string code in message)
+            {
+                var zeroes = code.Where(a => a == char.Parse("0")).Count();
+                int zeroesSum = zeroes * 3;
+                var ones = code.Where(a => a == char.Parse("1")).Count();
+                int onesSum = ones * 5;
+                sum = zeroesSum + onesSum;
+                int consecutiveDigitsSum = GetConsecutiveDigitsSum(code);
+                if (consecutiveDigitsSum>= 1)
+                {
+                    sum += consecutiveDigitsSum;
+                }
+                result += (char)sum;
+            }
+            Console.WriteLine(result);
+        }
+
+        static int GetConsecutiveDigitsSum(string code)
+        {
+            string pattern = @"1{2,}|0{2,}";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(code);
+            int count = 0;
+            foreach (Match match in matches)
+            {
+                count += match.Value.Length;
+            }
+            return count;
+        }
+    }
+}
