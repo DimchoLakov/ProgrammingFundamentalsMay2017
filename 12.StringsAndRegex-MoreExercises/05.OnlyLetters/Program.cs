756c4017bc4c1e949a03c6c7efb6b80bcd8ab722
whywherewhat <mdsektor@gmail.com>
2017-09-06 20:21:33 +0100

    Added 05.OnlyLetters

---
+++ 12.StringsAndRegex-MoreExercises/05.OnlyLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _05.OnlyLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Regex regex = new Regex(@"\d+");
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                int index = input.IndexOf(match.Value) + match.Value.Length;
+                try
+                {
+                    input = input.Replace(match.Value, input[index].ToString());
+                }
+                catch (Exception e)
+                {
+                    
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
