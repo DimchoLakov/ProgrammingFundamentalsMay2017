feaa4990930a9cb57c05545cc838540326acb9e0
whywherewhat <mdsektor@gmail.com>
2017-09-09 15:42:22 +0100

    Added 08.Mines

---
+++ 12.StringsAndRegex-MoreExercises/08.Mines/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _08.Mines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int startIndex = 0;
+            int endIndex = 0;
+
+            while ((startIndex = input.IndexOf("<", startIndex)) != -1 && (endIndex = input.IndexOf(">", startIndex)) != -1)
+            {
+                string chars = input.Substring(startIndex + 1, 2);
+
+                int power = Math.Abs(chars[0] - chars[1]);
+
+                int leftIndex = Math.Max(0, startIndex - power);
+                int rightIndex = Math.Min(input.Length - 1, endIndex + power);
+
+                StringBuilder result = new StringBuilder(input);
+
+                for (int i = leftIndex; i <= rightIndex; i++)
+                {
+                    result[i] = '_';
+                }
+                input = result.ToString();
+
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
