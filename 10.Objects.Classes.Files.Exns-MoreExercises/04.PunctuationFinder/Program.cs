82008747733bfb9017bb63aee045c9c74071a88d
Dimcho Lakov <mdsektor@yahoo.com>
2017-09-16 08:12:32 +0100

    Added 04.PunctuationFinder

---
+++ 10.Objects.Classes.Files.Exns-MoreExercises/04.PunctuationFinder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _04.PunctuationFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = File.ReadAllLines(@"C:\text-test\sample_text.txt");
+            char[] punctuationSymbols = new[] {':', ',', '.', '!', '?'};
+            List<char> result = new List<char>();
+
+            foreach (var line in text)
+            {
+                foreach (var c in line)
+                {
+                    if (punctuationSymbols.Contains(c))
+                    {
+                        result.Add(c);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
