32f34a8fddf03533b03ad4425199bec44dbafe15
Dimcho Lakov <mdsektor@yahoo.com>
2017-09-16 08:10:14 +0100

    Added 08.PersonalException

---
+++ 10.Objects.Classes.Files.Exns-MoreExercises/08.PersonalException/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _08.PersonalException
+{
+    class NegativeNumberException : Exception
+    {
+        public NegativeNumberException() : base($"My first exception is awesome!!!")
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (number >= 0)
+                    {
+                        Console.WriteLine(number);
+                    }
+                    else
+                    {
+                        throw new NegativeNumberException();
+                    }
+                }
+            }
+            catch (NegativeNumberException myExcp)
+            {
+                Console.WriteLine(myExcp.Message);
+            }
+        }
+    }
+}
